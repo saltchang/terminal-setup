853fcdd95d46cf01c147dfd1d67901879b8c9383
Salt Chang <saltchang.tw@gmail.com>
2021-11-14 03:43:23 +0800

    feat: update update-git script

--- terminal/scripts/update-git.sh
+++ terminal/scripts/update-git.sh
@@ -1,12 +1,27 @@
 #! /bin/bash
 
-printf "\nCurrent %s\n\n" "$(git --version)"
+# --------> Quickly update Git (for Ubuntu) ------------------------------------
+update_git() {
+    printf "\nCurrent %s\n\n" "$(git --version)"
 
-printf "Start to update Git...\n\n"
+    printf "Start to update Git...\n\n"
 
-sudo add-apt-repository ppa:git-core/ppa -y &&
-    sudo apt-get update &&
-    sudo apt-get install git -y
+    sudo add-apt-repository ppa:git-core/ppa -y &&
+        sudo apt-get update &&
+        sudo apt-get install git -y
 
-printf "\nCurrent %s\n\n" "$(git --version)"
-printf "Git updated successfully.\n"
+    printf "\nCurrent %s\n\n" "$(git --version)"
+    printf "Git updated successfully.\n"
+}
+
+# --------> Quickly update Git (for macOS) ------------------------------------
+update_git() {
+    printf "\nCurrent %s\n\n" "$(git --version)"
+
+    printf "Start to update Git...\n\n"
+
+    brew update && brew upgrade git
+
+    printf "\nCurrent %s\n\n" "$(git --version)"
+    printf "Git updated successfully.\n"
+}
